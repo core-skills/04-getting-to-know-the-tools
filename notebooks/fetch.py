639611c321d4a81f3df1feac69c40ab71150822d
Morgan Williams <morgan.j.williams@hotmail.com>
2020-11-09 21:55:36 +0800

    Add example data report

--- notebooks/fetch.py
+++ notebooks/fetch.py
@@ -1,8 +1,35 @@
+import base64
 import pandas as pd
 from pathlib import Path
 from io import BytesIO
 from zipfile import ZipFile
 from urllib.request import urlopen
+
+
+def get_onedrive_directlink(onedrive_link):
+    """
+    Get a public Onedrive filehandle for linking directly to pandas.
+    
+    Parameters
+    -----------
+    onedrive_link : str
+        Onedrive share URL.
+        
+    Returns
+    -------
+    direct_url
+        Base64-encoded URL for direct download/for use as a file handle.
+        
+    Notes
+    -----
+    Found here:
+    https://towardsdatascience.com/how-to-get-onedrive-direct-download-link-ecb52a62fee4
+    """
+    data_bytes64 = base64.b64encode(bytes(onedrive_link, 'utf-8'))
+    data_bytes64_string = data_bytes64.decode('utf-8').replace('/','_').replace('+','-').rstrip("=")
+    direct_url = "https://api.onedrive.com/v1.0/shares/u!{}/root/content".format(data_bytes64_string)
+    return direct_url
+
 
 def _fetch_UCI_dataset(url, usecache=True):
     """
@@ -34,6 +61,7 @@ def _fetch_UCI_dataset(url, usecache=True):
         df.to_pickle(cacheloc, compression='gzip')
     return df
 
+
 def fetch_beijing_AQ_data():
     """
     Fetch the Beijing Air Quality Dataset from the University of California Irvine Machine Learning Repository.
@@ -50,6 +78,7 @@ def fetch_beijing_AQ_data():
     """
     url = "https://archive.ics.uci.edu/ml/machine-learning-databases/00501/PRSA2017_Data_20130301-20170228.zip"
     return _fetch_UCI_dataset(url)
+
 
 def fetch_flue_gas_data():
     """
